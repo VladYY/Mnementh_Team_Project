0dd9a45e18488808b6e350f375760b6b6c1a46d4
Dido Viktorov <d.viktorovchak@gmail.com>
2017-02-10 22:38:57 +0200

    Enemy - crazy commit

--- src/Controller.java
+++ src/Controller.java
@@ -8,14 +8,20 @@ public class Controller {
 
     Game game;
     Fire TempFire;
-    Enemy TempEnemy;
+    Enemy enemy;
 
     public Controller(Game game){
 
         this.game = game;
     }
 
+    public void createEnemy(int enemy_count){
 
+        for (int i = 0; i < enemy_count; i++) {
+            addEnemy(new Enemy(100, 270, this.game));
+        }
+
+    }
 
     public void tick(){
 
@@ -27,7 +33,9 @@ public class Controller {
 
         for (int i = 0; i < e.size(); i++) {
 
+            enemy = e.get(i);
 
+            enemy.tick();
         }
     }
 
@@ -39,16 +47,16 @@ public class Controller {
         }
 
         for (int i = 0; i < e.size(); i++) {
-            TempEnemy = e.get(i);
+            enemy = e.get(i);
 
-            TempEnemy.render(graphics);
+            enemy.render(graphics);
         }
     }
 
-    public void createEnemy(Enemy block){
+
+    public void addEnemy(Enemy block){
         e.add(block);
     }
-
 
     public void addFire(Fire block){
         f.add(block);
