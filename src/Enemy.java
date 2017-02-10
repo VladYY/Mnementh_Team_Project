a4cf839a377388b80006c831e7d10a684e2e93c6
Dido Viktorov <d.viktorovchak@gmail.com>
2017-02-10 20:11:58 +0200

    add class Enemy

---
+++ src/Enemy.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+
+public class Enemy {
+    double x, y;
+
+    public Enemy(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void tick() {
+
+    }
+
+    public void render (Graphics g) {
+
+    }
+}
+
