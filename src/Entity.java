c2f96e7cfb9942c2e552975ede317092e116fac0
gogbog <gogbog11@gmail.com>
2017-02-11 17:22:52 +0200

    Added Entity

--- src/Entity.java
+++ src/Entity.java
@@ -4,10 +4,10 @@ import java.awt.*;
  * Created by George on 11-Feb-17.
  */
 public interface Entity {
-    public void tick();
-    public void render(Graphics g);
+    void tick();
+    void render(Graphics g);
 
-    public double getX();
-    public double getY();
+    double getX();
+    double getY();
 
 }
---
+++ src/GameObject.java
@@ -0,0 +1,6 @@
+/**
+ * Created by George on 11-Feb-17.
+ */
+public class GameObject {
+
+}
