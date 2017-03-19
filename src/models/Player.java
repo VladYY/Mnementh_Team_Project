4654f46fb50ff6a2118caca601c440f567882638
Dido Viktorov <d.viktorovchak@gmail.com>
2017-03-19 12:12:02 +0200

    Setting animation of Plsyer Class
    Setting animation of Player class using for loop for different images of
    the animation.

--- src/models/Player.java
+++ src/models/Player.java
@@ -147,23 +147,17 @@ public class Player extends DefaultObject implements FriendlyEntity {
     }
 
     private void setAnimation() {
-        this.dragon[0] = this.spriteSheet.grabDragonImage(1, 1, 72, 72);
-        this.dragon[1] = this.spriteSheet.grabDragonImage(2, 1, 72, 72);
-        this.dragon[2] = this.spriteSheet.grabDragonImage(3, 1, 72, 72);
-        this.dragon[3] = this.spriteSheet.grabDragonImage(4, 1, 72, 72);
-        this.dragon[4] = this.spriteSheet.grabDragonImage(5, 1, 72, 72);
-        this.dragon[5] = this.spriteSheet.grabDragonImage(6, 1, 72, 72);
-        this.dragon[6] = this.spriteSheet.grabDragonImage(7, 1, 72, 72);
-        this.dragon[7] = this.spriteSheet.grabDragonImage(8, 1, 72, 72);
+        int colIndex = 1;
+        int rowIndex = 1;
+        for (int i = 0; i < this.dragon.length; i++) {
+            if(i == 8){
+                colIndex = 1;
+                rowIndex = 2;
+            }
+            this.dragon[i] = this.spriteSheet.grabDragonImage(colIndex, rowIndex, 72, 72);
+            colIndex++;
 
-        this.dragon[8] = this.spriteSheet.grabDragonImage(1, 2, 72, 72);
-        this.dragon[9] = this.spriteSheet.grabDragonImage(2, 2, 72, 72);
-        this.dragon[10] = this.spriteSheet.grabDragonImage(3, 2, 72, 72);
-        this.dragon[11] = this.spriteSheet.grabDragonImage(4, 2, 72, 72);
-        this.dragon[12] = this.spriteSheet.grabDragonImage(5, 2, 72, 72);
-        this.dragon[13] = this.spriteSheet.grabDragonImage(6, 2, 72, 72);
-        this.dragon[14] = this.spriteSheet.grabDragonImage(7, 2, 72, 72);
-        this.dragon[15] = this.spriteSheet.grabDragonImage(8, 2, 72, 72);
+        }
 
         this.animationLeft = new Animation(5,
             this.dragon[8],
